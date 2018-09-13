de8d9f1378d14b18aec340894900b9b81969e26b
kamilpociot <kamil.pociot@me.com>
2018-09-13 19:49:05 +0200

    another cors attempt

--- src/main/java/com/pociot/accounts/AccountsApplication.java
+++ src/main/java/com/pociot/accounts/AccountsApplication.java
@@ -2,9 +2,13 @@ package com.pociot.accounts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import reactor.core.publisher.Mono;
 
 @SpringBootApplication
@@ -14,8 +18,25 @@ public class AccountsApplication {
     SpringApplication.run(AccountsApplication.class, args);
   }
 
+  @Bean
+  CorsWebFilter corsFilter() {
+
+    CorsConfiguration config = new CorsConfiguration();
+
+    config.setAllowCredentials(true);
+    config.addAllowedOrigin("https://accounting-kpociot.herokuapp.com/");
+    config.addAllowedOrigin("http://localhost:3000");
+    config.addAllowedHeader("*");
+    config.addAllowedMethod("*");
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", config);
+
+    return new CorsWebFilter(source);
+  }
+
   @RestController
-  @CrossOrigin(origins = "http://localhost:3000")
+  @CrossOrigin(origins = {"http://localhost:3000", "https://accounting-kpociot.herokuapp.com"})
   public class MyController {
 
     @GetMapping("hello")
