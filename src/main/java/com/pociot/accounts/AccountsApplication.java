cb38e1256f7e0628b7e1c4bab25972347df79f4c
kamilpociot <kamil.pociot@me.com>
2018-09-13 20:39:01 +0200

    hello world completed

--- src/main/java/com/pociot/accounts/AccountsApplication.java
+++ src/main/java/com/pociot/accounts/AccountsApplication.java
@@ -2,13 +2,9 @@ package com.pociot.accounts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsWebFilter;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import reactor.core.publisher.Mono;
 
 @SpringBootApplication
@@ -17,23 +13,6 @@ public class AccountsApplication {
   public static void main(String[] args) {
     SpringApplication.run(AccountsApplication.class, args);
   }
-//
-//  @Bean
-//  CorsWebFilter corsFilter() {
-//
-//    CorsConfiguration config = new CorsConfiguration();
-//
-//    config.setAllowCredentials(true);
-//    config.addAllowedOrigin("https://accounting-kpociot.herokuapp.com");
-////    config.addAllowedOrigin("http://localhost:3000");
-//    config.addAllowedHeader("*");
-//    config.addAllowedMethod("*");
-//
-//    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//    source.registerCorsConfiguration("/**", config);
-//
-//    return new CorsWebFilter(source);
-//  }
 
   @RestController
   public class MyController {
