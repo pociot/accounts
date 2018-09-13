45b0f050bbab996e1602ed5aa6e414d5faa13f1a
kamilpociot <kamil.pociot@me.com>
2018-09-13 20:31:41 +0200

    another cors try, one of the last..

--- src/main/java/com/pociot/accounts/AccountsApplication.java
+++ src/main/java/com/pociot/accounts/AccountsApplication.java
@@ -17,23 +17,23 @@ public class AccountsApplication {
   public static void main(String[] args) {
     SpringApplication.run(AccountsApplication.class, args);
   }
-
-  @Bean
-  CorsWebFilter corsFilter() {
-
-    CorsConfiguration config = new CorsConfiguration();
-
-    config.setAllowCredentials(true);
-    config.addAllowedOrigin("https://accounting-kpociot.herokuapp.com");
-//    config.addAllowedOrigin("http://localhost:3000");
-    config.addAllowedHeader("*");
-    config.addAllowedMethod("*");
-
-    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    source.registerCorsConfiguration("/**", config);
-
-    return new CorsWebFilter(source);
-  }
+//
+//  @Bean
+//  CorsWebFilter corsFilter() {
+//
+//    CorsConfiguration config = new CorsConfiguration();
+//
+//    config.setAllowCredentials(true);
+//    config.addAllowedOrigin("https://accounting-kpociot.herokuapp.com");
+////    config.addAllowedOrigin("http://localhost:3000");
+//    config.addAllowedHeader("*");
+//    config.addAllowedMethod("*");
+//
+//    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//    source.registerCorsConfiguration("/**", config);
+//
+//    return new CorsWebFilter(source);
+//  }
 
   @RestController
   public class MyController {
