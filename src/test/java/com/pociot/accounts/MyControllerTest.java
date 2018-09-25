83bc60524bde2771e0fddf55e0c0a44b1b205a6f
kamilpociot <kamil.pociot@me.com>
2018-09-25 17:47:31 +0200

    code coverage compliance

--- src/test/java/com/pociot/accounts/MyControllerTest.java
+++ src/test/java/com/pociot/accounts/MyControllerTest.java
@@ -2,12 +2,23 @@ package com.pociot.accounts;
 
 import static org.junit.Assert.*;
 
+import java.util.concurrent.ExecutionException;
 import org.junit.Test;
 
 public class MyControllerTest {
 
+  private final MyController myController;
+
+  public MyControllerTest(MyController myController) {
+    this.myController = myController;
+  }
+
   @Test
   public void getHelloWorld() {
-    assertTrue(true);
+    try {
+      assertEquals("Hello World!", myController.getHelloWorld().toFuture().get());
+    } catch (InterruptedException | ExecutionException e) {
+      e.printStackTrace();
+    }
   }
 }
