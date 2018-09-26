f51a9cd3369027089cde1eccc2d1da35e7dcdf66
kamilpociot <kamil.pociot@me.com>
2018-09-26 18:54:17 +0200

    changed eureka settings

--- src/main/java/com/pociot/accounts/AccountsApplication.java
+++ src/main/java/com/pociot/accounts/AccountsApplication.java
@@ -2,10 +2,10 @@ package com.pociot.accounts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class AccountsApplication {
 
   public static void main(String[] args) {
